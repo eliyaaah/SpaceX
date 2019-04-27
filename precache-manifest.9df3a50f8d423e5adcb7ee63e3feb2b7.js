a47bfcef14c5f7b130661849a1a4149fbfc59164
Illia Hladush <illia.gladush@gmail.com>
2019-04-27 14:39:24 -0400

    Updates

---
+++ precache-manifest.9df3a50f8d423e5adcb7ee63e3feb2b7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f12da6cdb26f8d62822995905c00e08e",
+    "url": "/SpaceX/index.html"
+  },
+  {
+    "revision": "752e558679edbacc5857",
+    "url": "/SpaceX/static/css/main.6591dccf.chunk.css"
+  },
+  {
+    "revision": "ba1f53734555a6becf91",
+    "url": "/SpaceX/static/js/2.2fe82bb7.chunk.js"
+  },
+  {
+    "revision": "752e558679edbacc5857",
+    "url": "/SpaceX/static/js/main.35ce8b9f.chunk.js"
+  },
+  {
+    "revision": "5d5afb4d5ede57da06f3",
+    "url": "/SpaceX/static/js/runtime~main.b2e78016.js"
+  },
+  {
+    "revision": "4c89d5a368168d2243f82ee726280211",
+    "url": "/SpaceX/static/media/SpaceX-logo.4c89d5a3.jpg"
+  },
+  {
+    "revision": "1f29971fcaa02816997c9703499a8fb8",
+    "url": "/SpaceX/static/media/background.1f29971f.jpg"
+  },
+  {
+    "revision": "d40abd656dd64e78eb6b4464996b07e5",
+    "url": "/SpaceX/static/media/youtube_logo.d40abd65.png"
+  }
+]);
